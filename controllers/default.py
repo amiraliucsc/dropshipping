1a303460889bd8d83c6eb36600307f57678a874b
SalvorMallow <brianjones86@gmail.com>
2016-12-02 23:07:17 -0800

    fixed product

--- controllers/default.py
+++ controllers/default.py
@@ -247,7 +247,7 @@ def product():
 
 def get_product(product_id):
 
-    query = "select * from product_view where product_id = '%s' where [default] = 1"% product_id
+    query = "select * from product_view where product_id = '%s' and [default] = 1"% product_id
     product = db.executesql(query, as_dict=True)[0]
     return product
 
