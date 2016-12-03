bca1dbda969ca12cd1b939b3ad9a5df4509515ae
victorsh91 <victor.sh.91@gmail.com>
2016-12-03 03:29:50 -0800

    finished connect add cart popup to data
    connected data to popup add cart

--- static/js/share.js
+++ static/js/share.js
@@ -42,6 +42,16 @@ $('.add-to-cart').on('click',function (e) {
 	//***** NEED TO CHECK FOR DUPLICATE ITEMS IN CART
 	var product_id = e.target.id;
 
+	var cart_title = $("#p_title"+ product_id).html();
+	var cart_image = "http://localhost:8000/dropshipping/static/images/Product/" + $("#p_img"+ product_id).html() + ".jpg";
+	var cart_desc = $("#p_desc"+ product_id).html();
+	var cart_price = $("#p_price"+ product_id).html();
+	$("#pop_img").html('<img src="'+cart_image+'" alt="Owl Image" width="100%"/>');
+	$("#pop_title").html('<h2>'+cart_title+'</h2>');
+	$("#pop_desc").html('<h4>'+ cart_desc+'</h4>');
+	//$("#pop_rating").html();
+	$("#pop_price").html('<h3>'+ cart_price+'</h3>');
+
 	var qty = 1  // Change it later
 	 $.ajax({
 		 type: "POST",
