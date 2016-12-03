847b0fb4a9972baf72c158119892e02187dce5ab
victorsh91 <victor.sh.91@gmail.com>
2016-12-03 00:25:46 -0800

    Fixed add-to-cart text interference, unstyled login
    removed strong

--- static/js/share.js
+++ static/js/share.js
@@ -38,7 +38,7 @@ jQuery(function(){
 });
 
 $('.add-to-cart').on('click',function (e) {
-	alert_add_cart('hello'); //linked from cartfuncjs.js
+	alert_add_cart(); //linked from cartfuncjs.js
 
 	var product_id = e.target.id;
 	var qty = 1  // Change it later
